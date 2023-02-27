6ee3c65aeb90d28cc292cade0b207265f25c8012
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2023-02-27 09:00:40 +0530

    Calculator

---
+++ calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    char ch;
+    printf("press+ for add\n");
+    printf("press-for sub\n");
+    printf("print*for multiply\n");
+    printf("press/for division\n");
+    printf("enter two numbers\n");
+    scanf("%d,%d",&a,&b);
+    printf("enter your choice");
+    fflush(stdin);
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case '+':c=a+b;
+                 break;
+        case '-':c=a-b;
+                 break;
+        case'*':c=a*b;
+                break;
+        case'/':c=a/b;
+                break;
+    }
+    printf("result id %D",c);
+    
+}
+
